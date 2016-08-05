2c544fae50be3d25ae7243b3f948a8b071b8ee06
Martin Hochel <hochelmartin@gmail.com>
2016-08-05 12:42:47 +0200

    feat(styles): add css modules + postcss + cssnext support

---
+++ src/global.d.ts
@@ -0,0 +1,8 @@
+declare module "*.css" {
+  var styles: any;
+  export = styles;
+}
+declare module "*.html" {
+  var html: string;
+  export = html;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 
+import './styles.css';
 import { App } from './App';
 
 render(<App />, document.getElementById('app'));
